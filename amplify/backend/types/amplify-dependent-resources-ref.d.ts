27e7e419579ea58ebf8273c4bf2a6d49eee03e5d
Tey Villanueva <justine090794@gmail.com>
2025-11-15 16:20:42 +0800

    add amplify test

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "bms": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
